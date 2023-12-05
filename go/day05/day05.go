5740733a062d00b558b79b5257031ea53fa5a3f8
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-12-05 10:53:02 -0300

    implement get seeds

--- go/day05/day05.go
+++ go/day05/day05.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Seeds []int
+
+var NewSeedsValidationError = errors.New("invalid input")
+
+func NewSeeds(input string) (seeds Seeds, err error) {
+	pattern := regexp.MustCompile("seeds: ([\\d\\s]+)")
+	if match := pattern.FindStringSubmatch(input); len(match) == 0 {
+		seeds = Seeds{}
+		err = NewSeedsValidationError
+	} else {
+		maybeSeeds := strings.Split(match[1], " ")
+		err = nil
+		for _, maybeSeed := range maybeSeeds {
+			if seed, err := strconv.Atoi(maybeSeed); err == nil {
+				seeds = append(seeds, seed)
+			}
+		}
+	}
+	return
+}
+
+func splitInput(input string) []string {
+	return strings.Split(input, "\n\n")
+}
