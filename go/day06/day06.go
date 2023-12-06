c5ccc505cbc97cb4e7bd69bd88c6f62afc8089fb
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-12-06 15:58:30 -0300

    implement parse input day 06

--- go/day06/day06.go
+++ go/day06/day06.go
@@ -1,12 +1,24 @@
 package main
 
+import (
+	"adventOfCode/utils"
+	"strings"
+)
+
 type Race struct {
 	Time     int
 	Distance int
 }
 
 func parseInput(input string) []Race {
-	// timesAndDistancesString := strings.Split(input, "\n")
-	// times
-	return []Race{}
+	timesAndDistancesString := strings.Split(input, "\n")
+	times := utils.ParseIntList(strings.Split(timesAndDistancesString[0], ":")[1])
+	distances := utils.ParseIntList(strings.Split(timesAndDistancesString[1], ":")[1])
+
+	races := []Race{}
+	for i, time := range times {
+		races = append(races, Race{Time: time, Distance: distances[i]})
+	}
+
+	return races
 }
