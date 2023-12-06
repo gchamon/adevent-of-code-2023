65cd5ffb2f14d5ae30950125634483e7992c33c3
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2023-12-06 16:42:22 -0300

    implement analytical GetWaysToWin

--- go/day06/day06.go
+++ go/day06/day06.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"adventOfCode/utils"
+	"fmt"
+	"math"
 	"strings"
 )
 
@@ -11,7 +13,22 @@ type Race struct {
 }
 
 func (r Race) GetWaysToWin() int {
-	return 0
+	fmt.Printf("%+v\n", r)
+	getBound := func(c float64) float64 {
+		return (float64(r.Time) + c*math.Sqrt(math.Pow(float64(r.Time), 2)-4*float64(r.Distance))) / 2
+	}
+	getBoundInt := func(bound float64, round func(float64) float64, c int) int {
+		boundRound := round(bound)
+		if bound == boundRound {
+			return int(bound) + c
+		} else {
+			return int(boundRound)
+		}
+	}
+	timeLowerBound := getBound(-1)
+	timeUpperBound := getBound(+1)
+	fmt.Println(timeUpperBound, timeLowerBound)
+	return getBoundInt(timeUpperBound, math.Floor, -1) - getBoundInt(timeLowerBound, math.Ceil, 1) + 1
 }
 
 func parseInput(input string) []Race {
